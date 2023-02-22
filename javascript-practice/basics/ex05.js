ef623d472e12bf76519cc4866e8c7d7dbe07a207
pt807 <pt807@naver.com>
2023-02-22 13:58:46 +0900

    js object#1

---
+++ javascript-practice/basics/ex05.js
@@ -0,0 +1,34 @@
+/*
+구문(statement)
+1. 자바스크립트 실행단위
+2. 구성요소
+    값, 연산식: 표현식
+    주석: 구문이지만 실행하지 않는다.
+    예약어: if, else, for
+3. 구문의 예
+    if, if~else
+    while, do~while, for
+    for~in
+    switch
+    변수 정의
+4. 공백: 토큰 구분, 무시
+5. 세미클론:
+*/
+
+// ;(세미클론)하는 역활을 표현식을 표현식 구문으로 만들어 바로 실행하게끔한다.
+s = "hello world"; console.log(s)
+
+// 1. 개행은 ;(세미클론)으로 대체된다.
+i = 10;
+console.log(i)
+
+// 2. 하지만 상황에 따라서는 토큰을 분리하는 역활도 한다.
+a
+=
+2
++
+3
+console.log(a)
+
+cons
+ole.log(a)
