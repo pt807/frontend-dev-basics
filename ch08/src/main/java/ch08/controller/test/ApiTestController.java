e0424660d93fb2ed8ea330875f2544f36e7806f7
pt807 <pt807@naver.com>
2023-02-28 10:33:25 +0900

    ajax #4

--- ch08/src/main/java/ch08/controller/test/ApiTestController.java
+++ ch08/src/main/java/ch08/controller/test/ApiTestController.java
@@ -6,6 +6,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/test")
 public class ApiTestController {
+
+	@RequestMapping("/text")
+	public String text() {
+		return "text";
+	}
+
+	@RequestMapping("/html")
+	public String html() {
+		return "html";
+	}
+
+	@RequestMapping("/xml")
+	public String xml() {
+		return "xml";
+	}
+
 	@RequestMapping("/json01")
 	public String json01() {
 		return "json01";
