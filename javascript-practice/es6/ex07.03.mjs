ce7aa0547b0496bbee2a83d6b8c9457504957366
pt807 <pt807@naver.com>
2023-03-06 14:51:29 +0900

    es6 #3

---
+++ javascript-practice/es6/ex07.03.mjs
@@ -0,0 +1,15 @@
+/**
+ *
+ * Named ExportI
+ *
+ * 1. exports(require.js)와 유사하다.
+ * 2. destructing이 가능하다.
+ *
+ */
+export const add = function (a, b) {
+  return a + b;
+};
+
+export const substract = function (a, b) {
+  return a - b;
+};
